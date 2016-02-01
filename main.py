39a64f10b35872cfda395275610754cc59dfb113
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-02-01 18:33:25 +0100

    using multiindices

--- main.py
+++ main.py
@@ -5,9 +5,26 @@ if __name__ == '__main__':
     pd.set_option("display.width", 300)
 
     from pyaddepar.reader import Reader
-    reader = Reader(id=aFirm, key=aKey, secret=aSecret)
-    print(reader.groups)
-    print(reader.positions())
-    print(reader.transactions(start=pd.Timestamp("2016-01-01")))
-    print(reader.entities())
 
+    reader = Reader(id=aFirm, key=aKey, secret=aSecret)
+
+    print(reader.positions())
+    print(reader.positions().ix["804328"])
+    print(reader.positions().xs("804328", level="Owner ID"))
+    print(reader.positions().xs("867344", level="Owned ID"))
+
+    print(reader.owner)
+    print(reader.products)
+    print(reader.transactions())
+    assert False
+
+    # print(reader.entities())
+
+
+    # p = reader.positions()
+    # print(p.keys())
+    # print(p)
+
+    # print(reader.entities())
+
+    print(reader.portfolio())
--- pyaddepar/reader.py
+++ pyaddepar/reader.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import requests
+import logging
 from csv import reader
 
 
 class Reader(object):
-    def __init__(self, id, key, secret):
+    def __init__(self, id, key, secret, logger=None):
         self.__headers = {'Addepar-Firm': id, 'Addepar-Api-Key': key, 'Addepar-Api-Secret': secret}
         self.__address = "https://api.addepar.com/partner/export/csv/"
+        self.__logger = logger or logging.getLogger(__name__)
 
     @staticmethod
     def __toFrame(r):
@@ -22,10 +24,15 @@ class Reader(object):
     def __format(t):
         return t.strftime("%Y-%m-%d")
 
+    #todo: Needs refactoring, one element can be in multiple groups
     @property
     def groups(self):
         r = requests.get(self.__address + "groups", headers=self.__headers)
-        return self.__toFrame(r)
+        b = self.entities()["Name"]
+        b = pd.Series(index=b.values, data=b.index)
+        a = self.__toFrame(r).set_index(keys=["Member Name"])
+        return pd.concat((a, b), join="inner", axis=1)
+
 
     @property
     def contacts(self):
@@ -37,16 +44,27 @@ class Reader(object):
         start = start or pd.Timestamp("1900-01-01")
         params = {"start_date": self.__format(start), "end_date": self.__format(end)}
         r = requests.get(self.__address + "entities", headers=self.__headers, params=params)
-        return self.__toFrame(r)
+        return self.__toFrame(r).set_index("ID")
 
     def positions(self, date=None):
         date = date or pd.Timestamp("today")
         params = {"date": self.__format(date)}
         r = requests.get(self.__address + "positions", headers=self.__headers, params=params)
-        return self.__toFrame(r)
+        return self.__toFrame(r).set_index(keys=["Owner ID", "Owned ID"])
 
-    def transactions(self, start, end=None):
+    def transactions(self, start=None, end=None):
         end = end or pd.Timestamp("today")
+        start = start or pd.Timestamp("1900-01-01")
         params = {"start_date": self.__format(start), "end_date": self.__format(end)}
         r = requests.get(self.__address + "transactions", headers=self.__headers, params=params)
-        return self.__toFrame(r)
+        return self.__toFrame(r).set_index(keys=["Transaction ID","Type","Posted Date","Date","Owner ID", "Owned ID"])
+
+    @property
+    def owner(self):
+        ids = self.positions().index.get_level_values(level=0).unique()
+        return(self.entities().ix[ids])
+
+    @property
+    def products(self):
+        ids = self.positions().index.get_level_values(level=1).unique()
+        return(self.entities().ix[ids])
