79240df0b2e907a78cd12096360e02826265aacc
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-05-09 16:44:41 +0200

    figi demo file

---
+++ figi.py
@@ -0,0 +1,6 @@
+from auth import figiKey
+from pyaddepar.figi import figi
+
+if __name__ == '__main__':
+    print(figi(idValue="CARSECC", idType="TICKER", openfigi_apikey=figiKey))
+    print(figi(idValue="BBG000CBHLD2", idType="ID_BB_GLOBAL", openfigi_apikey=figiKey))
