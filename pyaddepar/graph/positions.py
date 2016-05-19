54b0704622951f07871e693e104a1aaa460adca7
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-05-19 15:26:20 +0200

    position graph

--- pyaddepar/graph/positions.py
+++ pyaddepar/graph/positions.py
@@ -1,45 +1,36 @@
 import networkx as nx
+import pandas as pd
 
 
 class Positions(object):
     """
-    Groups are described by a bipartite graph. With Group vs. Members. A member can be in multiple groups.
-    A member can not be a group and a group can not be member of a different group.
+    Positions are not a bipartite graph. An owner can be owned by a different owner. We model this as a directed graph.
     """
-
     def __init__(self, data):
-        self.__data = data
+        self.__data = data.sortlevel(level=0)
         self.__dg = nx.DiGraph()
-        print(data)
 
         for ids, d in data.iterrows():
             self.__dg.add_node(ids[0])
             self.__dg.add_node(ids[1])
             self.__dg.add_edge(ids[0], ids[1], attr_dict=d.to_dict())
 
-    def __getitem__(self, item):
-        return self.__dg.node[item]
-
-    def owns(self, id):
-        return self.__dg.successors(id)
-
-    def owned(self, id):
-        return self.__dg.predecessors(id)
-
     @property
-    def nodes(self):
-        return self.__dg.nodes()
+    def data(self):
+        return self.__data
 
-    def edge(self, id_owner, id_owns):
-        return self.__dg[id_owner][id_owns]
+    def __edge(self, owner, owns):
+        return self.__data.ix[(owner, owns)]
 
-    @property
-    def reachable(self):
-        return {node: list(nx.dfs_postorder_nodes(self.__dg, node)) for node in self.nodes}
+    def __edges(self, id):
+        """ List of all edges reachable from node id """
+        return pd.concat([self.__edge(owner=e[0], owns=e[1]) for e in nx.dfs_edges(self.__dg, id)], axis=0)
 
-    @property
-    def edges(self):
-        return {node: [edge for edge in nx.dfs_edges(self.__dg, node)] for node in self.nodes}
+    def owns(self, ids):
+        assert isinstance(ids, list)
+        # all edges reachable from a list of nodes
+        return pd.concat([self.__edges(id) for id in ids], axis=0)
 
     def __repr__(self):
         return self.__data.to_string()
+
