94f4993961b5979d8e77a0ccece0a4494c6271c4
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-05-20 13:01:00 +0200

    modelling the groups

--- pyaddepar/graph/groups.py
+++ pyaddepar/graph/groups.py
@@ -1,4 +1,4 @@
-import networkx as nx
+import pandas as pd
 
 
 class Groups(object):
@@ -6,39 +6,33 @@ class Groups(object):
     Groups are described by a bipartite graph. With Group vs. Members. A member can be in multiple groups.
     A member can not be a group and a group can not be member of a different group.
     """
+    @staticmethod
+    def __verify_name(names):
+        x = names[0]
+        for name in names:
+            assert name == x
+        return x
+
     def __init__(self, data):
         self.__data = data
-        self.__dg = nx.DiGraph()
+        self.__groups = [group for group in data.index.get_level_values(level="Group ID").unique()]
+        self.__owns = pd.Series({group: list(self.__data.xs(group, level="Group ID").index) for group in self.__groups})
+        self.__name = pd.Series({group: Groups.__verify_name(self.__data.xs(group, level="Group ID")["Group Name"])
+                                 for group in self.__groups})
 
-        for ids, x in data.iterrows():
-            self.__dg.add_node(ids[0], attr_dict={"name": x["Group Name"]}, bipartite=0)
-            self.__dg.add_node(ids[1], attr_dict={"name": x["Member Name"]}, bipartite=1)
-            self.__dg.add_edge(ids[0], ids[1])
-
-        self.__bottom_nodes, self.__top_nodes = nx.bipartite.sets(self.__dg)
-        self.__owns = {group: self.__dg.successors(group) for group in self.__bottom_nodes}
-        self.__owned = {group: self.__dg.predecessors(group) for group in self.__top_nodes}
+        assert len(self.__name.unique()) == len(self.__name)
 
     @property
     def ids(self):
-        return {group: self[group]["name"] for group in self.__bottom_nodes}
+        return self.__name
 
     @property
     def names(self):
-        return {self[group]["name"]: group for group in self.__bottom_nodes}
-
-    def __getitem__(self, id):
-        return self.__dg.node[id]
+        return pd.Series({v: k for k, v in self.ids.items()})
 
     @property
     def owns(self):
         return self.__owns
 
-    @property
-    def owned(self):
-        return self.__owned
-
     def __repr__(self):
         return self.__data.to_string()
-
-
