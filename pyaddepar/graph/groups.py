91df699c7daedc9a2ea10d97199077b361fd7440
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-05-23 09:44:36 +0200

    make groups a dictionary of groups

--- pyaddepar/graph/groups.py
+++ pyaddepar/graph/groups.py
@@ -1,7 +1,13 @@
-import pandas as pd
+class Group(object):
+    def __repr__(self, *args, **kwargs):
+        return "Group {0} owns {1}".format(self.name, self.owns)
+
+    def __init__(self, name, owns):
+        self.name = name
+        self.owns = owns
 
 
-class Groups(object):
+class Groups(dict):
     """
     Groups are described by a bipartite graph. With Group vs. Members. A member can be in multiple groups.
     A member can not be a group and a group can not be member of a different group.
@@ -13,41 +19,24 @@ class Groups(object):
             assert name == x
         return x
 
-    def __init__(self, data):
-        self.__data = data
-        self.__groups = [group_id for group_id in data.index.get_level_values(level="Group ID").unique()]
-        self.__owns = pd.Series({group_id: list(self.__data.xs(group_id, level="Group ID").index) for group_id in self.__groups})
-        self.__name = pd.Series({group_id: Groups.__verify_name(self.__data.xs(group_id, level="Group ID")["Group Name"])
-                                 for group_id in self.__groups})
+    def __init__(self, data, **kwargs):
+        super().__init__(**kwargs)
+        groups = [group_id for group_id in data.index.get_level_values(level="Group ID").unique()]
+        name = {group_id: Groups.__verify_name(data.xs(group_id, level="Group ID")["Group Name"]) for group_id in groups}
+        owns = {group_id: list(data.xs(group_id, level="Group ID").index) for group_id in groups}
+        self.__name = {group_id: Group(name=name[group_id], owns=owns[group_id]) for group_id in groups}
 
-        assert len(self.__name.unique()) == len(self.__name)
+    def __getitem__(self, item):
+        return self.__name[item]
 
-    @property
-    def groups(self):
-        return self.__groups
+    def items(self):
+        return self.__name.items()
 
-    @property
-    def name(self):
-        """
-        Series {group_id_i: name_i}
-        :return:
-        """
-        return self.__name
+    def values(self):
+        return self.__name.values()
 
-    @property
-    def group_id(self):
-        """
-        Series {name_i: group_id_i}
-        :return:
-        """
-        return pd.Series({v: k for k, v in self.name.items()})
+    def keys(self):
+        return self.__name.keys()
 
-    @property
-    def group_holdings(self):
-        """
-        Series {group_id_i : [member_1, ...]}
-        :return:
-        """
-        #todo: I wish there would be a simple method to link from member_1 to account_1_of_member_1, account_2_of...
-        #todo: This would make connecting with the transaction table a lot simpler
-        return self.__owns
+    def __iter__(self):
+        return self.__name.__iter__()
