701ca79706723dc131d2e94b21c974b67dbf9462
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-06-15 14:05:58 +0200

    subpackage missing in setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!./env/bin/python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pyaddepar',
     version='v0.3.00',
-    packages=["pyaddepar"],
+    packages=find_packages(include=["pyaddepar*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
     description='', install_requires=['requests>=2.9.1', 'pandas>=0.18.0', 'networkx>=1.11']
