6edd0517e9144614056b6697e6fc257aff584b33
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-05-11 16:32:06 +0200

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-docker build --file Dockerfile-Test --tag pyaddepar:latest .
+docker build --file Dockerfile-Test --tag pyaddepar:test .
 
 # run all tests, seems to be slow on teamcity
-docker run --rm pyaddepar:latest
+docker run --rm -v $(pwd)/html-coverage/:/html-coverage pyaddepar:test
 
 ret=$?
+
+docker rmi pyaddepar:test
 
 exit $ret
