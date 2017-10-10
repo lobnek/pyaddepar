4a4a711eb1b9bfa7316585b858663ce69a548497
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-10-10 16:04:43 +0200

    Docker cleaning

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM continuumio/miniconda3
 # File Author / Maintainer
 MAINTAINER Thomas Schmelzer "thomas.schmelzer@lobnek.com"
 
-RUN conda install -q -y pandas=0.19.1 requests networkx
+RUN conda install -q -y pandas requests networkx
 
 ADD ./pyaddepar /pyaddepar/pyaddepar
 ADD ./scripts   /pyaddepar/scripts
--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     packages=find_packages(include=["pyaddepar*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
-    description='', install_requires=['requests>=2.9.1', 'pandas>=0.18.0', 'networkx>=1.11']
+    description='', install_requires=['requests>=2.12.0', 'pandas>=0.20.0', 'networkx>=1.11']
 )
