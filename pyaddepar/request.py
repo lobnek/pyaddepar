1da2e7232d061b3292af5bd728eb7643706142a1
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-04-19 14:26:53 +0200

    fixing tests

--- pyaddepar/request.py
+++ pyaddepar/request.py
@@ -42,17 +42,18 @@ class Request(object):
     def headers(self):
         return {"content-type": "application/vnd.api+json", "Addepar-Firm": self.id}
 
-    def get(self, request, headers=None):
-        if headers:
-            h = {**self.headers, **headers}
-        else:
-            h = self.headers
+    def get(self, request):
+        #, headers=None):
+        #if headers:
+        #    h = {**self.headers, **headers}
+        #else:
+        h = self.headers
 
         r = "https://{company}.addepar.com/api/v1/{request}".format(request=request, company=self.company)
         self.logger.debug("Request: {request}, Headers: {headers}".format(request=r, headers=h))
         r = requests.get(r, auth=(self.key, self.secret), headers=h)
         assert r.ok, "Invalid response. Statuscode {}".format(r.status_code)
-        return r.json()
+        return r
 
     @property
     def version(self):
@@ -73,13 +74,14 @@ class Request(object):
                           "start_date": start_date.strftime("%Y-%m-%d"),
                           "end_date": end_date.strftime("%Y-%m-%d")})
 
-        return self.get(request=request)
+        self.logger.debug("Request: {request}".format(request=request))
+        return self.get(request=request).json()
 
     def entity(self, id=None):
         if id:
-            return self.get("entities/{id}".format(id=id))
+            return self.get("entities/{id}".format(id=id)).json()
         else:
-            return self.get("entities")
+            return self.get("entities").json()
 
     def group(self, id=None):
         if id:
@@ -87,5 +89,53 @@ class Request(object):
         else:
             return self.get("groups")
 
-    def group_member(self, id):
-        return self.get("groups/{id}/members".format(id=id))
+    #def group_member(self, id):
+    #    return self.get("groups/{id}/members".format(id=id))
+
+
+    def post_file(self, new_name, name):
+        #h = {"Addepar-Firm": self.id}
+        r = "https://{company}.addepar.com/api/v1/{request}".format(request="files", company=self.company)
+
+        files = {'file': (new_name, open(name, "rb"))}
+        r = requests.post(r, auth=self.auth, headers=self.headers, files=files)
+
+        assert r.ok, "Invalid response. Statuscode {}".format(r.status_code)
+        return r.json()["data"]
+
+
+    def files(self, id=None):
+        if id:
+            return self.get("files/{id}".format(id=id)).json()
+        else:
+            x = self.get("files").json()
+            return {a["id"] : a for a in x["data"]}
+
+
+    def file_download(self, id, file=None):
+        r =  self.get("files/{id}/download".format(id=id))
+        if file:
+            with open(file, "wb") as f:
+                f.write(r.content)
+
+    def file_groups(self, id):
+        return self.get("files/{id}/associated_groups".format(id=id)).json()
+
+    def file_entities(self, id):
+        return self.get("files/{id}/associated_entities".format(id=id)).json()
+    #GET / v1 / files /: file - id / associated_groups
+
+    def file_delete(self, id):
+        #r = "https://{company}.addepar.com/api/v1/{request}".format(request=request, company=self.company)
+        #self.logger.debug("Request: {request}, Headers: {headers}".format(request=r, headers=h))
+        #DELETE / v1 / files /: file - id
+        r = requests.delete("https://{company}.addepar.com/api/v1/files/{id}".format(id=id, company=self.company),  auth=self.auth, header=self.headers)
+        assert r.ok, "Invalid response. Statuscode {}".format(r.status_code)
+        #r = requests.get(r, auth=(self.key, self.secret), headers=h)
+
+
+if __name__ == '__main__':
+    y = Request().post_file(new_name="test10.csv", name="/data/report.csv")
+    Request().file_delete(id=y["id"])
+    Request().file_download(id=y["id"], file="/data/graph8.csv")
+
