5d5afe4547e49706585eaaeebdbbe8e526de9499
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-06-29 12:27:14 +0200

    updating pandas

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyaddepar',
-    version='0.5.5',
+    version='0.5.6',
     packages=find_packages(include=["pyaddepar*"]),
     author='Lobnek Wealth Management',
     author_email='thomas.schmelzer@lobnek.com',
