ab4467774c8ce1ed4f5611c6fa98cd33218d1d0f
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-08-17 08:07:09 +0200

    docker-compose test

--- test.sh
+++ test.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-docker build --file Dockerfile --target test --tag pyaddepar:test .
-
-# delete all files in html-coverage
-rm -rf $(pwd)/html-coverage/*
-
-# run all tests, seems to be slow on teamcity
-docker run --rm -v $(pwd)/html-coverage/:/html-coverage -v $(pwd)/html-report:/html-report pyaddepar:test
-
-ret=$?
-
-docker run --rm -v $(pwd)/source:/pyaddepar/source:ro -v $(pwd)/build:/pyaddepar/build pyaddepar:test sphinx-build source build
-
-docker rmi pyaddepar:test
-
-exit $ret
+docker-compose -f docker-compose.test.yml build --no-cache
+docker-compose -f docker-compose.test.yml run test-pyaddepar
+docker-compose -f docker-compose.test.yml run test-pyaddepar sphinx-build source build
