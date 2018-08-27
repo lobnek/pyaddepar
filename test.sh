a65120f71b9aac8c96908adc908e511d5a43c031
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-08-27 17:54:32 +0200

    graph and readonly

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose.test.yml build --no-cache
 docker-compose -f docker-compose.test.yml run test-pyaddepar
-docker-compose -f docker-compose.test.yml run test-pyaddepar sphinx-build source build
+docker-compose -f docker-compose.test.yml run test-pyaddepar sphinx-build /source /build
