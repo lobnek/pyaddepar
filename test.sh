617ab6cb5fcf8fead7f603118557164a00a172f5
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-08-28 14:59:19 +0200

    cleaning after test

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose.test.yml build --no-cache
+./graph.sh
 docker-compose -f docker-compose.test.yml run test-pyaddepar
 docker-compose -f docker-compose.test.yml run test-pyaddepar sphinx-build /source /build
+
+# remove all containers that are exited...
+docker rm $(docker ps -q -f status=exited)
+
+# remove the volumes hanging around...
+docker volume prune -f
+
