a42d705b54b9072b1040d69ece396ded3fffd01e
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-08-29 12:43:07 +0200

    cleaning after test

--- test.sh
+++ test.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose.test.yml build
-./graph.sh
-
 docker-compose -f docker-compose.test.yml run test-pyaddepar
 ret=$?
-
-docker-compose -f docker-compose.test.yml run test-pyaddepar sphinx-build /source artifacts/build
 
 # removes also all the containers linked to this particular service "test" defined in "docker-compose.test.yml"
 docker-compose -f docker-compose.test.yml rm -v -f test
