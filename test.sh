9c17a46a2b438c6af105e987f695314441d35bd8
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-09-03 12:36:34 +0200

    cleaning after test

--- test.sh
+++ test.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose.test.yml build
 docker-compose -f docker-compose.test.yml run test-pyaddepar
-ret=$?
-
-# removes also all the containers linked to this particular service "test" defined in "docker-compose.test.yml"
-docker-compose -f docker-compose.test.yml rm -v -f test
-
-exit $ret
