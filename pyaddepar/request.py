342845aaa47c53cc49b384b426b0b45babf43cf4
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2018-12-14 11:35:58 +0100

    csv experiments

--- pyaddepar/request.py
+++ pyaddepar/request.py
@@ -4,6 +4,7 @@ from enum import Enum
 
 import pandas as pd
 import requests
+import io
 
 
 class OutputType(Enum):
@@ -19,16 +20,11 @@ class PortfolioType(Enum):
     ENTITY = "entity"
 
 
-def addepar2frame(json, index="name"):
-    x = json["data"]["attributes"]["total"]["children"]
-    frame = pd.DataFrame({i: {**{index: a[index]}, **a["columns"]} for i, a in enumerate(x)}).transpose()
-    names = {a["key"]: a["display_name"] for a in json["meta"]["columns"]}
-    return frame.rename(columns=lambda x: names[x] if x in names.keys() else x)
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
-def view2frame(r):
-    import io
-    x = io.BytesIO(r.content)
-    return pd.read_csv(x)
 
 class Request(object):
     def __init__(self, key=None, secret=None, id=None, company=None, logger=None):
@@ -46,132 +42,106 @@ class Request(object):
     def headers(self):
         return {"content-type": "application/vnd.api+json", "Addepar-Firm": self.id}
 
-    def get(self, request):
-        h = self.headers
+    @staticmethod
+    def dicturl(d):
+        return '&'.join(["{key}={value}".format(key=key, value=value) for key, value in d.items()])
 
+    def get(self, request):
         r = "https://{company}.addepar.com/api/v1/{request}".format(request=request, company=self.company)
-        self.logger.debug("Request: {request}, Headers: {headers}".format(request=r, headers=h))
-        r = requests.get(r, auth=(self.key, self.secret), headers=h)
+        self.logger.debug("Request: {request}, Headers: {headers}".format(request=r, headers=self.headers))
+        r = requests.get(r, auth=(self.key, self.secret), headers=self.headers)
         assert r.ok, "Invalid response. Statuscode {}".format(r.status_code)
         return r
 
     @property
     def version(self):
-        return self.get("api_version")
+        return self.get("api_version").json()
 
-    def view(self, view_id, portfolio_id, portfolio_type, start_date=(pd.Timestamp("today")),
-             end_date=pd.Timestamp("today")):
+    # def view(self, view_id, portfolio_id, portfolio_type, start_date=pd.Timestamp("today"),
+    #          end_date=pd.Timestamp("today")):
+    #
+    #     assert isinstance(portfolio_type, PortfolioType)
+    #
+    #     param = Request.dicturl({"portfolio_id": portfolio_id, "portfolio_type": portfolio_type.value,
+    #                              "output_type": OutputType.JSON.value, "start_date": start_date.strftime("%Y-%m-%d"),
+    #                              "end_date": end_date.strftime("%Y-%m-%d")})
+    #
+    #     request = "portfolio/views/{view}/results?{param}".format(view=view_id, param=param)
+    #
+    #     self.logger.debug("Request: {request}".format(request=request))
+    #     return self.get(request=request).json()
 
-        def __dict(d):
-            return '&'.join(["{key}={value}".format(key=key, value=value) for key, value in d.items()])
-
-        assert isinstance(portfolio_type, PortfolioType)
-
-        request = "portfolio/views/{view}/results?".format(view=view_id) + \
-                  __dict({"portfolio_id": portfolio_id,
-                          "portfolio_type": portfolio_type.value,
-                          "output_type": OutputType.JSON.value,
-                          "start_date": start_date.strftime("%Y-%m-%d"),
-                          "end_date": end_date.strftime("%Y-%m-%d")})
-
-        self.logger.debug("Request: {request}".format(request=request))
-        return self.get(request=request).json()
-
-    def view_csv(self, view_id, portfolio_id, portfolio_type, start_date=(pd.Timestamp("today")),
-             end_date=pd.Timestamp("today")):
-
-        def __dict(d):
-            return '&'.join(["{key}={value}".format(key=key, value=value) for key, value in d.items()])
+    def view_csv(self, view_id, portfolio_id, portfolio_type, start_date=pd.Timestamp("today"),
+                 end_date=pd.Timestamp("today")):
 
         assert isinstance(portfolio_type, PortfolioType)
 
-        request = "portfolio/views/{view}/results?".format(view=view_id) + \
-                  __dict({"portfolio_id": portfolio_id,
-                          "portfolio_type": portfolio_type.value,
-                          "output_type": OutputType.CSV.value,
-                          "start_date": start_date.strftime("%Y-%m-%d"),
-                          "end_date": end_date.strftime("%Y-%m-%d")})
+        param = Request.dicturl({"portfolio_id": portfolio_id, "portfolio_type": portfolio_type.value,
+                                 "output_type": OutputType.CSV.value, "start_date": start_date.strftime("%Y-%m-%d"),
+                                 "end_date": end_date.strftime("%Y-%m-%d")})
+
+        request = "portfolio/views/{view}/results?{param}".format(view=view_id, param=param)
 
         self.logger.debug("Request: {request}".format(request=request))
-        return self.get(request=request)
+        return pd.read_csv(io.BytesIO(self.get(request=request).content))
 
     def entity(self, id=None):
         if id:
-            return self.get("entities/{id}".format(id=id)).json()
+            return AttrDict(self.get("entities/{id}".format(id=id)).json()["data"]["attributes"])
         else:
-            return self.get("entities").json()
+            return {a["id"]: AttrDict(a["attributes"]) for a in self.get("entities").json()["data"]}
 
-    def group(self, id=None):
-        if id:
-            return self.get("groups/{id}/members".format(id=id))
-        else:
-            return self.get("groups")
-
-    def post_file(self, new_name, name):
-        # h = {"Addepar-Firm": self.id}
-        r = "https://{company}.addepar.com/api/v1/{request}".format(request="files", company=self.company)
-
-        files = {'file': (new_name, open(name, "rb"))}
-        r = requests.post(r, auth=self.auth, headers=self.headers, files=files)
-
-        assert r.ok, "Invalid response. Statuscode {}".format(r.status_code)
-        return r.json()["data"]
-
-    def files(self, id=None):
-        if id:
-            return self.get("files/{id}".format(id=id)).json()
-        else:
-            x = self.get("files").json()
-            return {a["id"]: a for a in x["data"]}
-
-    def file_download(self, id, file=None):
-        r = self.get("files/{id}/download".format(id=id))
-        if file:
-            with open(file, "wb") as f:
-                f.write(r.content)
-
-    def file_groups(self, id):
-        return self.get("files/{id}/associated_groups".format(id=id)).json()
-
-    def file_entities(self, id):
-        return self.get("files/{id}/associated_entities".format(id=id)).json()
-
-    # GET / v1 / files /: file - id / associated_groups
-
-    def file_delete(self, id):
-        # r = "https://{company}.addepar.com/api/v1/{request}".format(request=request, company=self.company)
-        # self.logger.debug("Request: {request}, Headers: {headers}".format(request=r, headers=h))
-        # DELETE / v1 / files /: file - id
-        r = requests.delete("https://{company}.addepar.com/api/v1/files/{id}".format(id=id, company=self.company),
-                            auth=self.auth, header=self.headers)
-        assert r.ok, "Invalid response. Statuscode {}".format(r.status_code)
-        # r = requests.get(r, auth=(self.key, self.secret), headers=h)
+    #def group(self, id=None):
+    #    if id:
+    #        return self.get("groups/{id}/members".format(id=id)).json()["data"]
+    #    else:
+    #        return {a["id"]: AttrDict(a["attributes"]) for a in self.get("groups").json()["data"]}
 
     def users(self, id=None):
         if id:
-            return self.get("users/{id}".format(id=id)).json()["data"]
+            return AttrDict(self.get("users/{id}".format(id=id)).json()["data"]["attributes"])
         else:
-            return {a["id"]: a for a in self.get("users").json()["data"]}
+            return {a["id"]: AttrDict(a["attributes"]) for a in self.get("users").json()["data"]}
 
 
-if __name__ == '__main__':
-    r = Request().view_csv(view_id=53676, portfolio_id=6076, portfolio_type=PortfolioType.GROUP)
-    import io
-    x = io.BytesIO(r.content)
-    print(x)
-    f = pd.read_csv(x, index_col=["Top Level Owner [Entity ID]", "Top Level Owner"])
-    # do the renaming
+    # def post_file(self, new_name, name):
+    #     # h = {"Addepar-Firm": self.id}
+    #     r = "https://{company}.addepar.com/api/v1/{request}".format(request="files", company=self.company)
+    #
+    #     files = {'file': (new_name, open(name, "rb"))}
+    #     r = requests.post(r, auth=self.auth, headers=self.headers, files=files)
+    #
+    #     assert r.ok, "Invalid response. Statuscode {}".format(r.status_code)
+    #     return r.json()["data"]
+    #
+    # def files(self, id=None):
+    #     if id:
+    #         return self.get("files/{id}".format(id=id)).json()
+    #     else:
+    #         x = self.get("files").json()
+    #         return {a["id"]: a for a in x["data"]}
+    #
+    # def file_download(self, id, file=None):
+    #     r = self.get("files/{id}/download".format(id=id))
+    #     if file:
+    #         with open(file, "wb") as f:
+    #             f.write(r.content)
+    #
+    # def file_groups(self, id):
+    #     return self.get("files/{id}/associated_groups".format(id=id)).json()
+    #
+    # def file_entities(self, id):
+    #     return self.get("files/{id}/associated_entities".format(id=id)).json()
+    #
+    # # GET / v1 / files /: file - id / associated_groups
+    #
+    # def file_delete(self, id):
+    #     # r = "https://{company}.addepar.com/api/v1/{request}".format(request=request, company=self.company)
+    #     # self.logger.debug("Request: {request}, Headers: {headers}".format(request=r, headers=h))
+    #     # DELETE / v1 / files /: file - id
+    #     r = requests.delete("https://{company}.addepar.com/api/v1/files/{id}".format(id=id, company=self.company),
+    #                         auth=self.auth, header=self.headers)
+    #     assert r.ok, "Invalid response. Statuscode {}".format(r.status_code)
+    #     # r = requests.get(r, auth=(self.key, self.secret), headers=h)
 
-    f = f.reset_index()
-    print(f)
 
-    offset=300
-    date = pd.Timestamp("today")
-    f = view2frame(Request().view_csv(view_id=49702, portfolio_id=1, portfolio_type=PortfolioType.FIRM, start_date=date - pd.offsets.Day(n=offset), end_date=date))
-
-    #x = io.BytesIO(r.content)
-    #print(x)
-    #f = pd.read_csv(x)
-
-    print(f)
-    print(f.keys())
