a27e4d0347ce215a12dc09a49f0da5784e13c6f6
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2020-03-09 13:47:42 +0100

    testing with flask

--- pyaddepar/addeparrequest.py
+++ pyaddepar/addeparrequest.py
@@ -126,9 +126,11 @@ class AddeparRequest(object):
 
         param = AddeparRequest.dicturl({"portfolio_id": portfolio_id, "portfolio_type": portfolio_type.value,
                                  "output_type": OutputType.CSV.value, "start_date": start_date.strftime("%Y-%m-%d"),
-                                 "end_date": end_date.strftime("%Y-%m-%d"), "addepar-firm": self.id})
+                                 "end_date": end_date.strftime("%Y-%m-%d"), "addepar_firm": self.id})
 
+        print(param)
         request = "/v1/transactions/views/{view}/results?{param}".format(view=view_id, param=param)
+        print(request)
 
         self.logger.debug("Request: {request}".format(request=request))
         return pd.read_csv(io.BytesIO(self.get(request=request).content))
